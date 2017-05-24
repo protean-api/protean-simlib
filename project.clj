3db0b8a1c40a3ad4ac34175c33ae600016b2e2b3
rossputin <rossajmcd@gmail.com>
2017-05-24 17:59:53 +0100

    update protean-api dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [protean-api "0.11.0-pre.6"]]
+                 [protean-api "0.11.0"]]
   :plugins [[lein-expectations "0.0.8"]]
   :aot :all
   :uberjar-name ~(str (-> "project.clj" slurp read-string (nth 1)) "-" (-> "project.clj" slurp read-string (nth 2)) "-standalone.jar"))
