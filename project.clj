c152bc01c696a9a7f3c2387d0305b78e4de27d8f
nick.featch@gmail.com <nick.featch@gmail.com>
2017-05-31 22:32:08 +0100

    remove uberjar config

--- project.clj
+++ project.clj
@@ -7,5 +7,4 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [protean-api "0.11.0"]]
   :plugins [[lein-expectations "0.0.8"]]
-  :aot :all
-  :uberjar-name ~(str (-> "project.clj" slurp read-string (nth 1)) "-" (-> "project.clj" slurp read-string (nth 2)) "-standalone.jar"))
+  :aot :all)
