50cda2338931695a69c613118320691354c75792
nick.featch@gmail.com <nick.featch@gmail.com>
2017-05-31 22:30:38 +0100

    bump version to 0.12.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protean-simlib "0.11.0"
+(defproject protean-simlib "0.12.0"
   :description "Protean Sample Sim Library."
   :url "http://github.com/protean-api/protean-simlib"
   :license {:name "Apache License v2.0"
