fcd858ac7edf5b8a8040b0c3c01786644e557f9a
Nick Featch <Nick.Featch@passivsystems.com>
2017-06-02 10:55:27 +0100

    bump protean-api dependency to 0.12.0-pre.1

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [protean-api "0.11.0"]]
+                 [protean-api "0.12.0-pre.1"]]
   :plugins [[lein-expectations "0.0.8"]]
   :aot :all)
