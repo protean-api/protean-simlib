fe4011e4f3a1e15a711d43f970a3a13ac1539e30
Nick Featch <nick.featch@gmail.com>
2020-06-22 18:14:35 +0100

    change project version from 0.14.0-SNAPSHOT to 0.14.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject protean-simlib "0.14.0-SNAPSHOT"
+(defproject protean-simlib "0.14.0"
   :description "Protean Sample Sim Library."
   :url "http://github.com/protean-api/protean-simlib"
   :license {:name "Apache License v2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [protean-api "0.14.0-SNAPSHOT"]]
+                 [protean-api "0.14.0"]]
   :plugins [[lein-expectations "0.0.8"]]
   :aot :all)
