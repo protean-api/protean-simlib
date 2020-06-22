ba01d094a4efad1fd45b3e6da485a00f964944fe
Nick Featch <nick.featch@gmail.com>
2020-06-22 18:08:03 +0100

    update dependenciese

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License v2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [protean-api "0.14.0-SNAPSHOT"]]
   :plugins [[lein-expectations "0.0.8"]]
   :aot :all)
